daaeca19362460fa7ae59058567ad2a2c1aec507
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-02-22 23:35:38 +0500

    Assignment-2

--- JavaScript/Assignments/Class-2-Assignment/app.js
+++ JavaScript/Assignments/Class-2-Assignment/app.js
@@ -2,4 +2,5 @@ let num1 = +prompt("Enter First Number ");
 let num2 = +prompt("Enter Second Number ");
 let num = num1 + num2;
 
+
 document.write("The First Number You Enter Is " + num1 + " And Second Number is " + num2 + " And The Number Of Addition Is " + num);
