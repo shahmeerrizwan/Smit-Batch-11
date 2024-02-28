dc6000d8b7034c9c195047a587b39512bf1e5cb2
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-02-29 02:48:03 +0500

    Alert-Assignment

---
+++ JavaScript/Assignments/Alert-Assignment/app.js
@@ -0,0 +1,38 @@
+// QUESTION NO 1 :
+
+// alert("Hello Buddy !!! ");
+
+
+// QUESTION NO 2 :
+
+// alert("Error! Please Enter a Valid Password.");
+
+
+//QUESTION NO 3 :
+
+// alert("Welcome To JS Land... \nHAPPY Cooding!");
+
+
+// QUESTION NO 4 :
+// PART   A :
+
+// alert("Welcome to JS Land...");
+
+
+// PART   B :
+
+// alert("Happy Cooding! \nPrevent this page from additional dialogs.")
+
+
+
+// QUESTION NO 5 :
+
+// alert("Hello... I can run JS through my browser's console");
+
+
+// QUESTION NO 6 :
+
+// alert("Hello World");
+
+
+// QUESTION 7 SOLVED IN HTML FILE 
