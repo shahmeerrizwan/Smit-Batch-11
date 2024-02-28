111366c6dc3880cdf3a4a8382a02639eb6025cc4
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-02-29 01:38:04 +0500

    03-Increment-Decrement

---
+++ JavaScript/03-Increment-Decrement/app.js
@@ -0,0 +1,34 @@
+// pre inc   1-> 2
+// post inc  1-> 2
+// pre dec   1-> 0
+// post dec  1-> 0
+
+// var num = 5
+
+// var eq = --num + num-- + num-- + --num + num
+//             // 4  +   4   +   3   +    1  +  1
+
+//        document.write(eq)
+
+// var num1 = 2
+// var num2 = 3
+// var eq = ++num1 + num2++ + num1++ + num2++ + --num2 + num2-- 
+//          + --num2 + num2-- + num1++ + ++num1
+
+
+// document.write(eq)
+
+
+
+
+var num1 = 2
+var num2 = 4
+var num3 = 5
+
+var eq = num1++ + ++num2 - 2 + --num3 + ++num3 - 10 - --num1
+2 + 5 - 2 + 4 + 5 - 10 - 2
+
+
+
+    - num1++ + ++num2 + 1 + ++num3 - num3++ - --num3 + 2
+    - 2 + 6 + 1 + 6 - 6 - 6 + 2
