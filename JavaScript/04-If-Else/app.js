2b283f353479c565f66d22fc3610c70dbe0fae2b
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-02-29 01:45:28 +0500

    04-If-Else

---
+++ JavaScript/04-If-Else/app.js
@@ -0,0 +1,177 @@
+// var a = 3
+// var b = 4
+// var res = a++ + ++a - b-- - --b + a++ + b++ - ++a + ++b
+// // 3 +  5  -  4  -   2 +  5  +   2 -  7  +   4
+// document.write(res)
+
+
+// var match = prompt("Enter value")
+
+// if(match == "ABCD"){
+//     alert("value is true")
+// }
+// else{
+//     alert("value is false")
+// }
+
+// var num1 = +prompt("Enter value 1")
+// var num2 = +prompt("Enter value 2")
+// var op = prompt("Enter operator")
+
+// if(op == "+"){
+//     document.write(num1 + num2)
+// }
+// else if(op == '-'){
+//     document.write(num1 - num2)
+// }
+
+// else if(op == '*'){
+//     document.write(num1 * num2)
+// }
+
+// else if(op == '/'){
+//     document.write(num1 / num2)
+// }
+
+// else if(op == '%'){
+//     document.write(num1 % num2)
+// }
+
+// var firstName = "Saad"
+// var lastName = "Ali"
+
+// document.write("first name is "+firstName+"<br>"+" last name is "+ lastName)
+
+// template literals
+
+// document.write(`first name is ${firstName} <br> and my last name is ${lastName}`)
+
+
+// var studentName = prompt("Enter Name ")
+// var english = +prompt("Enter english marks")
+// var maths = +prompt("Enter maths marks")
+// var urdu = +prompt("Enter urdu marks")
+// var fsx = +prompt("Enter fsx marks")
+// var chem = +prompt("Enter chem marks")
+// var total = english + maths + urdu + fsx + chem
+// var perc = total / 500 * 100
+// var grade;
+
+// if(perc >= 80){
+//     grade = "A+"
+// }
+// else if(perc >= 70){
+//     grade = "A"
+// }
+
+// else if(perc >= 60){
+//     grade = "B"
+// }
+
+// else if(perc >= 50){
+//     grade = "C"
+// }
+
+// else if(perc >= 40){
+//     grade = "D"
+// }
+// else{
+//     grade = "F"
+// }
+
+// document.write(
+//     `<table border='1px'>
+//     <tr>
+//     <th>STUDENT NAME</th>
+//     <th>ENGLISH</th>
+//     <th>MATHS</th>
+//     <th>URDU</th>
+//     <th>FSX</th>
+//     <th>CHEM</th>
+//     <th>TOTAL</th>
+//     <th>PERCENTAGE</th>
+//     <th>GRADE</th>
+//     </tr>
+
+//     <tr>
+//     <td>${studentName}</td>
+//     <td>${english}</td>
+//     <td>${maths}</td>
+//     <td>${urdu}</td>
+//     <td>${fsx}</td>
+//     <td>${chem}</td>
+//     <td>${total} / 500</td>
+//     <td>${perc.toFixed(2)}%</td>
+//     <td>${grade}</td>
+//     </tr>
+//     </table>`
+// )
+
+
+// var routerUserName = prompt("Enter email")
+// var routerPassword = prompt("Enter password")
+
+// if(routerUserName == "admin" || routerPassword == "123456"){
+//     alert("login success")
+// }
+// else{
+//     alert("login fail")
+// }
+
+// var num = 5
+// if(num === "5"){
+//     alert("okay")
+// }
+// else{
+//     alert("not okay")
+// }
+
+var num = 2
+
+if (num == 4) {
+    if (num > 1) {
+        alert("okay")
+    }
+}
+else {
+    alert("not okay")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(perc >= 80){
+//     document.write("A+")
+// }
+// else if(perc >= 70){
+//     document.write("A")
+// }
+
+// else if(perc >= 60){
+//     document.write("B")
+// }
+
+// else if(perc >= 50){
+//     document.write("C")
+// }
+
+// else if(perc >= 40){
+//     document.write("D")
+// }
+// else{
+//     document.write("FAIL nikammey ...")
+// }
+
+
