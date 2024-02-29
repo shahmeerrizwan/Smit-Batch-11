fa94122d390a7697695ee23b6cb10cc2fc202262
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-01 00:54:12 +0500

    Variables-Assignment

---
+++ JavaScript/Assignments/Variables-Assignment/app.js
@@ -0,0 +1,70 @@
+// QUETION NO 1
+
+// let name = "username";
+// console.log(name);
+
+
+// QUETION NO 2
+
+// let myname = "MUHAMMAD SHAHMEER RIZWAN";
+// console.log(myname);
+
+
+// QUESTION NO 3
+
+// let message = "HELLO WORLD";
+// alert(message);
+
+
+// QUESTION NO 4
+
+
+// let name = "MUHAMMAD SHAHMEER RIZWAN";
+// let age = 20;
+// let institute = "SMIT";
+// let rollnumber = "WMA-20267";
+
+// alert("Name = " + name);
+// alert("Age = " + age);
+// alert("Institute Name = " + institute);
+// alert("Roll Number = " + rollnumber);
+
+
+
+// QUESTION NO 5
+
+
+// let name = "PIZZA";
+
+// alert(name)
+
+
+// SOLVED NHI KIYA
+
+
+
+
+// QUESTION NO 6
+
+
+// let job = "Software Developer";
+// let city = "Pakistan";
+// let partnername = "Amna Zia";
+// let kids = 2;
+
+// alert(`You Will be a ${job} in ${city}, and Married to ${partnername} with ${kids} kids. `)
+
+
+
+// QUESTION NO 7
+
+
+// let email = "abc@gmail.com";
+// alert(`My Email Address is ${email}`);
+
+
+
+// QUESTION NO 8
+
+// let book = "A Smarter Way to Learn Javascript";
+// alert(book);
