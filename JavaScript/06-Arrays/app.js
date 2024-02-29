88359651a6c52f872f51f983cc7120c1a4df780c
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-01 00:59:11 +0500

    06-Arrays

---
+++ JavaScript/06-Arrays/app.js
@@ -0,0 +1,73 @@
+// var num1 = 3
+// var num2 = 5
+
+// num1 += 10
+// num1 = 13
+
+
+// num1 -= 5
+// num1 = 8
+
+// num2 += 4
+// num2 = 9
+
+// num2 -= 10
+// num2 = -1
+
+// num1 = num2
+
+// document.write("the value of num1 is "+num1 + "<br>" + " and the value of num2 is "+ num2)
+
+// 8, -1
+// -1, -1
+
+
+// num1 = num1 + 5
+// num1 += 5
+// document.write(num1)
+// var eq = --num1 - --num1 + num2++ + ++num2 + num1++ + ++num1 + num1++ + --num2
+//    2    -    1   +   5    +    7   +   1    +    3   +   3    +    6
+// document.write(eq)
+// var a = 5
+// var b = 4
+// var eq = a+2 + a-- + b++ + b+10 + b-10 + a+4 + ++b + b++ + a
+//        7  +  5  +  4  +  15  +  -5  +  8  +  6  +   6 + 4
+// document.write(eq)
+// var eq = a++ + a++ + a+2 + b+10 + 10 + a++ + a++ + --b + b-20
+//           5  +  6  +  9  +  14  + 10 +  7  +  8  +  3  +   -17
+// var id = 1
+// var fname = "ali"
+// var lname = "abc"
+// var roll = "123"
+// var ins = "SMIT"
+
+// document.write(ins)
+
+
+
+
+// var arr = [1, "Ali", "abc", "CSC-201", "SMIT"]
+// arr.push("saylani","ABCD")
+
+// arr.pop()
+// arr.pop()
+// arr.pop()
+
+// arr.push("DEFG")
+
+// document.write(arr)
+
+// document.write(`Last name is ${arr[2]} and Institue is ${arr[4]}`)
+
+
+// var arr = [1, 2, 3, 4, 5, 6, "false", true, 18.2, "Saad"]
+
+// arr.pop()
+// arr.pop()
+// arr.pop()
+
+// document.write(arr[7])
+
+
+// var a;
+// document.write(a)
