512bf447b7b28b6247d43218346c1fdfa37ecedd
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-06 02:53:31 +0500

    VariablesNames-Legal-Illegal

---
+++ JavaScript/Assignments/Variables-Names-Legal-ILegal/app.js
@@ -0,0 +1,48 @@
+// QUESTION NO 1 :
+
+
+// let name = "shahmeer";
+// let age = 19;
+// let passion = "developr";
+
+
+// console.log(`My Name is ${name} & i Am ${age} Years Old .`);
+
+
+// QUESTION NO 2 :
+
+
+// LEGAL VARIABLES NAMES
+
+// let name = "shahmeer";
+// let age = 19;
+// let _num = 3243217819;
+// let phone = "Samsung";
+// let Email = "shahmeerrizwan921@gmail.com";
+
+// ILEGAL VARIABLES NAMES
+
+// let class = 90;
+// let true = "shahmer";
+// let my-variable = "shah";
+// let 123 = 320;
+// let first name = "shahmeer"'
+
+
+
+// QUESTION NO 3 :
+
+// document.write(`
+// <h1>“Rules for naming JS variables” </h1>
+// <br>
+// <ul>
+// <li>"Variable names can only contain letters, numbers, underscores, and dollar signs ($).",</li>
+// <li>"Variables must begin with a letter, underscore, or dollar sign ($).",</li>
+// <li>"Variable names are case-sensitive.",</li>
+// <li>"Variable names should not be JS reserved keywords.",</li>
+// </ul>
+// `)
+
+
+
+//                                   -- END --
