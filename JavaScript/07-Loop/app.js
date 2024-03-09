5a20374926b52e533a9ad1e1ac2312cc40b0c6cd
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-10 02:18:21 +0500

    07-Loop

---
+++ JavaScript/07-Loop/app.js
@@ -0,0 +1,57 @@
+// for (var index = 0; index < 5; index++) {
+//     document.write("hello world");
+// }
+
+// var arr = ["karachi", "lahore", "islamabad", "pindi"]
+// var cityName = prompt("Enter city name: ");
+// var cityFound = false;
+// for (var i = 0; i < arr.length; i++) {
+//     if (cityName == arr[i]) {
+//         cityFound = true
+//         alert("yes this city is one of the cleanest cities");
+//         break
+//     }
+// }
+// if (cityFound == false) {
+//     alert("city not found")
+// }
+
+
+
+
+
+// initialization
+// condition
+// inc / dec
+
+
+// var num = 5
+// num = num + 2
+// num += 4
+
+
+
+
+// var i;
+
+// for(i=0; i<=10; i = i+2){
+//     document.write(i+". smit"+"<br>")
+// }
+
+// for(var i=10; i > 0; i--){
+//     document.write(i, "<br>")
+// }
+
+for (var i = 1; i <= 10; i++) {
+    document.write(5 + " x " + i + " = " + 5 * i + "<br>")
+}
+
+
+
+
+
+
+
+
+
+
