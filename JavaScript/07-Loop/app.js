030bdeff67aba3dfaf309b68d8b6f7f6cfb730e7
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-10 02:19:02 +0500

    07-Loop

--- JavaScript/07-Loop/app.js
+++ JavaScript/07-Loop/app.js
@@ -48,6 +48,33 @@ for (var i = 1; i <= 10; i++) {
 
 
 
+// var sabzi = ["aloo", "piyaaz", "timatar", "Kheera", "Kaddu"]
+// for (var i = sabzi.length - 1; i >= 0; i--){
+//     document.write(sabzi[i],"<br>")
+// }
+
+
+// for(var i=1; i<= 100; i++){
+//     if(i % 2 == 0){
+//         document.write(i,"<br>")
+//     }
+// }
+
+// for(var i=10; i>0; i--){
+//     document.write(i,"<br>")
+// }
+
+
+// var arr = [3, 6, 2, 56, 32, 5, 9, 32];
+// var largest = arr[0];
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] > largest ) {
+//     largest = arr[i];
+//   }
+// }
+// console.log(largest);
+
 
 
 
