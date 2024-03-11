7faa0a9eade85e48ff3e967603088d3ad9099039
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-12 03:26:00 +0500

    if-else-statement

---
+++ JavaScript/Assignments/If-Else/app.js
@@ -0,0 +1,99 @@
+// QUESTION NO 1 :
+
+
+// let number = prompt("Enter any number ");
+
+// if (number.length == 0) {
+//     alert("Empty Input")
+// } else if (number % 3 == 0) {
+//     alert(number + " is divisible by 3 and divisible number is " + number / 3)
+// } else if (isNaN(number)) {
+//     alert("Enter a Valid Number")
+// } else {
+//     alert(number + " is not divisible by 3")
+// }
+
+
+
+// PRACTICE
+
+// let number = prompt("Enter Any Number");
+
+// if (number.length == 0) {
+//     alert("Empty Input")
+// } else if (number % 3 == 0) {
+//     alert(number + " is divisible by 3 and divisible number is " + number / 3)
+// } else if (isNaN(number)) {
+//     alert("Enter a Valid Number")
+// } else {
+//     alert(number + " is not divisible by 3")
+// }
+
+
+
+// QUESTION NO 2 :
+
+// let number = +prompt("Enter any number");
+
+// if (number % 2 == 0) {
+//     alert(number + " is Even")
+// } else if (isNaN(number)) {
+//     alert("Enter a Valid Number")
+// } else {
+//     alert(number + " is Odd")
+// }
+
+
+// QUESTION NO 3 :
+
+// let age = +prompt("Enter Your Age");
+// if (age == 0) {
+//     alert("Empty Input")
+// } else if (age >= 18) {
+//     alert(age + " is Too Old Enough")
+// } else if (isNaN(age)) {
+//     alert("Enter a Valid Number")
+// } else {
+//     alert(age + " is Too Young")
+// }
+
+
+
+
+// QUESTION NO 4 :
+
+// let name = prompt("Enter Your Name ");
+
+// if (name == "shahmeer") {
+//     alert("Hello, Shahmeer ! It's great to see you!")
+// }
+
+
+
+// QUESTION NO 5 :
+
+// Switch CASE
+
+
+// QUESTION NO 6 :
+
+// let input = prompt("Enter a character:");
+
+// if (input.length === 1) {
+//     let charCode = input.charCodeAt(0);
+
+//     if (charCode >= 48 && charCode <= 57) {
+//         console.log("The input is a number.");
+//     }
+//     else if (charCode >= 65 && charCode <= 90) {
+//         console.log("The input is an uppercase letter.");
+//     }
+//     else if (charCode >= 97 && charCode <= 122) {
+//         console.log("The input is a lowercase letter.");
+//     }
+//     else {
+//         console.log("The input is neither a number nor a letter.");
+//     }
+// } else {
+//     console.log("Invalid input. Please enter a single character.");
+// }
