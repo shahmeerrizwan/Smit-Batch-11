7e981792fdb58bd655c8fd29c2b91faec65fa1ae
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-18 04:36:08 +0500

    Conditional-Rendering

---
+++ JavaScript/Assignments/Conditional-Rendering/app.js
@@ -0,0 +1,22 @@
+// QUESTION NO 1 :
+
+
+// let name = prompt("Enter Your Name ");
+// alert(`Hi, ${name}`)
+
+
+
+// QUESTION NO 2 :
+
+
+// let number = prompt("Enter a number you want to generate table");
+// let table = "<h3>Multiplication Table of " + number + "</h3>";
+
+// if (number == "") {
+//     number = 5;
+// }
+// for (let i = 1; i <= 10; i++) {
+//     table += number + " × " + i + " = " + (number * i) + "<br>";
+// }
+
+// document.write(table);
