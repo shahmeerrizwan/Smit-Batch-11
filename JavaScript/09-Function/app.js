b27a90acd8ddcf36a98367cbaafeb699235bdc11
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-23 23:43:31 +0500

    09-Function

---
+++ JavaScript/09-Function/app.js
@@ -0,0 +1,80 @@
+// function abc(){
+//     alert("saylani")
+// }
+
+// abc()
+// abc()
+// abc()
+// abc()
+// abc()
+// abc()
+// abc()
+// abc()
+
+// function table(){
+//     for(var i=1; i<=10; i++){
+//         document.write(2 + " x "+ i + " = "+ 2*i+ "<br>")
+//     }
+// }
+// table()
+// table()
+// table()
+// table()
+// table()
+
+// function sum(a, b){
+//     return "saylani"
+// }
+
+// document.write(sum(10,5))
+
+// document.write(sum(20,20))
+
+
+// var a = +prompt("Enter value 1")
+// var b = +prompt("Enter value 2")
+// var c = prompt("Enter operator")
+// var sum;
+// function calc(a, b, c) {
+//     if (c == '+') {
+//         sum = a + b
+//     }
+//     else if (c == '-') {
+//         sum = a - b
+//     }
+//     else if (c == '*') {
+//         sum = a * b
+//     }
+//     else if (c == '/') {
+//         sum = a / b
+//     }
+//     return sum
+// }
+// document.write(calc(a,b,c))
+
+
+// var a = 5
+// function add(){
+//     a = 8
+//     console.log(a) // 8
+// }
+// add()
+// console.log(a) // 5
+
+
+
+// function another(){
+//     var b = 6
+//     console.log(b) // 6
+// }
+// another()
+// console.log(b) // 6
+
+
+
+function print(a, b) {
+
+    return a + b
+
+}
+
