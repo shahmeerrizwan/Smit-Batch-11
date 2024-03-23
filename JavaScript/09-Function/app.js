091c55dbf88411dc848d2a85754a36bfc7e65f39
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-03-23 23:45:47 +0500

    09-Function

--- JavaScript/09-Function/app.js
+++ JavaScript/09-Function/app.js
@@ -72,9 +72,48 @@
 
 
 
-function print(a, b) {
+// function print(a, b) {
 
-    return a + b
+//     return a + b
 
+// }
+
+
+
+
+
+
+// CLASS 2 
+
+
+// function factorial(a){
+//     var num = 1
+//     for(var i=2; i<=a; i++){
+//         num = num * i
+//     }
+//     return num
+// }
+// console.log(factorial(1))
+
+// function foo() {
+//     var newText = 'Lorem, ipsum dolor sit amet consectetur ipisicingelitLiberoiustoquosuscipit doloribus, rem similique vero nobis dolorum distinctio veritatis vitae, eos ratione.Porro earum eos necessitatibus sed, nisi delectus.'
+//     document.getElementById('para').innerHTML = newText
+//     document.getElementById('anc').innerText = ''
+// }
+
+function onBulb() {
+    var getBulb = document.getElementById('bulb')
+    getBulb.src = 'https://static.vecteezy.com/system/resources/previews/030/491/407/non_2x/glowing-electric-light-bulb-with-lightning-inside-3d-rendering-ai-generated-free-photo.jpg'
+    getBulb.setAttribute('width', '50%')
 }
+
+
+
+function offBulb() {
+    var getBulb = document.getElementById('bulb')
+    getBulb.src = 'https://i.pinimg.com/originals/e0/fd/25/e0fd25f9127a9a109a0648c83ee61643.png'
+    getBulb.setAttribute('width', '20%')
+}
+
+
 
