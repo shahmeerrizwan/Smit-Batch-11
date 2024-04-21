8aaacb53225a5ef2816a4f995a193dc90e486f06
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-04-21 10:44:31 +0500

    Blb-on-off

---
+++ JavaScript/Assignments/Bulb-On-Off/app.js
@@ -0,0 +1,15 @@
+function onBulb() {
+    let getBulb = document.getElementById("on")
+    getBulb.src = "https://www.freeiconspng.com/thumbs/lightbulb-png/light-bulb-png-bulb-png1247-12.png"
+
+    getBulb.setAttribute('width', '400px')
+    getBulb.setAttribute('height', '400px')
+}
+function offBulb() {
+    let getBulb = document.getElementById("on")
+    getBulb.src = "https://www.shutterstock.com/image-illustration/3d-illustration-light-bulb-isolated-600nw-749587456.jpg"
+
+    getBulb.setAttribute('width', '400px')
+    getBulb.setAttribute('height', '400px')
+
+}
