37d227fe6dbb7cb1bf52d813586a21cdfb8af9e8
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-05-02 23:43:21 +0500

    Stop-Watch

--- JavaScript/Assignments/Stop-Watch/app.js
+++ JavaScript/Assignments/Stop-Watch/app.js
@@ -1,17 +1,16 @@
 let milisec = 0;
 let seconds = 0;
 let minutes = 0;
+let myTimeInterval = null
 let getHtmlmsec = document.getElementById('msec');
 let getHtmlsec = document.getElementById('sec');
 let getHtmlmin = document.getElementById('min');
 
 
 function start() {
-
-
-    setInterval(function () {
+    myTimeInterval = setInterval(function () {
         milisec++;
-        getHtmlmsec.innerHTML = " : " + milisec + " milisec ";
+        getHtmlmsec.innerHTML = " : " + milisec + " msec ";
         if (milisec >= 100) {
             seconds++;
             getHtmlsec.innerHTML = " : " + seconds + " sec ";
@@ -27,3 +26,15 @@ function start() {
 }
 
 
+function stop() {
+    clearInterval(myTimeInterval);
+}
+
+function clearTimer() {
+    milisec = 0;
+    seconds = 0;
+    minutes = 0;
+    getHtmlmsec.innerHTML = " : 00 ";
+    getHtmlsec.innerHTML = " : 00 ";
+    getHtmlmin.innerHTML = "00 ";
+}
