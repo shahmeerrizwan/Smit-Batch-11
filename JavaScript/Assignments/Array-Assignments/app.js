eedc3d9d4875a006eb5d441cd0a668173ad41f46
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-05-02 23:57:27 +0500

    Array-Assignment

---
+++ JavaScript/Assignments/Array-Assignments/app.js
@@ -0,0 +1,73 @@
+//question 1
+
+//   let studentNames = [];
+
+//  studentNames.push("Abdullah");
+//  studentNames.push("Adnan");
+//  studentNames.push("Talha");
+
+//  console.log(studentNames);
+
+//question 2
+
+//  let studentNames = new Array();
+//  let studentNames = [];
+
+
+//question 3
+
+
+// let colors = ["red", "green", "blue", "yellow"];
+
+
+//question 4
+
+// let numbers = [1, 2, 3, 4, 5];
+
+
+//question 5
+
+//  let flags = [true, false, true, false, true];
+
+//question 7
+
+// let networks = ["Mobilink", "Telenor", "Zong", "Ufone", "Warid"];
+
+// console.log("Available mobile networks in Pakistan:");
+// for (let i = 0; i < networks.length; i++) {
+//     console.log(networks[i]);
+// }
+
+
+//question 8
+
+// let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// document.write("<h2>Available Education Qualifications in Pakistan:</h2>");
+// document.write("<ol>");
+
+// for (let i = 0; i < qualifications.length; i++) {
+//     document.write("<li>" + qualifications[i] + "</li>");
+// }
+
+// document.write("</ol>");
+
+
+//question 9
+
+// let topMovies2015 = [];
+
+// topMovies2015.push("Avengers: Age of Ultron");
+// topMovies2015.push("Star Wars: The Force Awakens");
+// topMovies2015.push("Jurassic World");
+// topMovies2015.push("Inside Out");
+// topMovies2015.push("Mad Max: Fury Road");
+
+// document.write("<h2>Top Movies of 2015:</h2>");
+// document.write("<ul>");
+// for (let i = 0; i < topMovies2015.length; i++) {
+//     document.write("<li>" + topMovies2015[i] + "</li>");
+// }
+// document.write("</ul>");
+
+// document.write("<p>Number of movies in the list: " + topMovies2015.length + "</p>");
