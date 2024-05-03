92a1e418e83aecb7ee1a679f2105958b4fa384c4
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-05-03 23:22:40 +0500

    String-Method-Assignments

---
+++ JavaScript/Assignments/String-Method-Assignments/app.js
@@ -0,0 +1,30 @@
+// QUESTION NO 1 :
+
+// let firstName = prompt("Enter your First Name");
+// let secondName = prompt("Enter Your Second Name");
+
+// alert(" Wellcome " + firstName + " " + secondName);
+
+
+
+// Question no 2 :
+
+
+// let favMobile = prompt("Enter Your Favourite Mobile Name");
+
+// console.log("My Favourite Phone is " + favMobile +
+//     "\n" + "Length Of String Is : " + favMobile.length)
+
+
+
+// Question no 3 :
+
+
+// let text = "pakistan";
+// console.log(text.indexOf("n"));
+
+
+// Question no 4 :
+
+// let text = "Hello World";
+// console.log(text.lastIndexOf("l"));
