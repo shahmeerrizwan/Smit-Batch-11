360e3044728b0b6f589d029fd885e90ac6dee218
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-05-30 03:23:07 +0500

    Quiz-App

--- JavaScript/Assignments/Quiz-Application/app.js
+++ JavaScript/Assignments/Quiz-Application/app.js
@@ -1,56 +1,69 @@
-
 var questions = [
     {
-        question: 'What does HTML stand for?',
+        question: 'Q 1 ) Which character is used to indicate an end tag ?',
+        option1: '*',
+        option2: '/',
+        option3: '<',
+        correctOption: '/'
+    },
+    {
+        question: '2 ) What is the correct HTML for creating a hyperlink ?',
+        option1: '<a>http://www.w3schools.com</a>',
+        option2: '<a href="http://www.w3schools.com">W3Schools</a>',
+        option3: '<a url="http://www.w3schools.com">W3Schools.com</a>',
+        correctOption: `<a href="http://www.w3schools.com">W3Schools</a>`
+    },
+    {
+        question: 'Q 3 )  What does HTML stand for?',
         option1: 'Hyperlinks and Text Markup Language',
         option2: 'Hypertext Markup Language',
         option3: 'Home Tool Markup Language',
         correctOption: "Hypertext Markup Language"
     },
     {
-        question: 'Who is making the Web standards?',
+        question: 'Q 4 ) Who is making the Web standards?',
         option1: 'Google',
         option2: 'The World Wide Web Consortium',
         option3: 'Microsoft',
         correctOption: "The World Wide Web Consortium"
     },
     {
-        question: 'Choose the correct HTML element for the largest heading:',
+        question: 'Q 5 ) Choose the correct HTML element for the largest heading:',
         option1: '<heading>',
         option2: '<h6>',
         option3: '<h1>',
         correctOption: "<h1>"
     },
     {
-        question: 'What is the correct HTML element for inserting a line break?',
+        question: 'Q 6 ) What is the correct HTML element for inserting a line break?',
         option1: '<linebreak>',
         option2: '<br>',
         option3: '<break>',
         correctOption: "<br>"
     },
     {
-        question: 'What is the correct HTML for adding a background color?',
+        question: 'Q 7 ) What is the correct HTML for adding a background color?',
         option1: '<body bg="yellow">',
         option2: '<background>yellow</background>',
         option3: '<body style="background-color:yellow;">',
         correctOption: '<body style="background-color:yellow;">'
     },
     {
-        question: 'Choose the correct HTML element to define important text:',
+        question: 'Q 8 ) Choose the correct HTML element to define important text:',
         option1: '<strong>',
         option2: '<b>',
         option3: '<i>',
         correctOption: '<strong>'
     },
     {
-        question: 'Choose the correct HTML element to define emphasized text:',
+        question: 'Q 9 ) Choose the correct HTML element to define emphasized text:',
         option1: '<italic>',
         option2: '<i>',
         option3: '<em>',
         correctOption: "<em>"
     },
     {
-        question: 'What is the correct HTML for creating a hyperlink?',
+        question: 'Q 10 ) What is the correct HTML for creating a hyperlink?',
         option1: '<a>http://www.w3schools.com</a>',
         option2: '<a href="http://www.w3schools.com">W3Schools</a>',
         option3: '<a url="http://www.w3schools.com">W3Schools.com</a>',
@@ -59,99 +72,56 @@ var questions = [
 ]
 
 
+var ques = document.getElementById('question');
+var index = 0;
+var btn = document.getElementById('btn');
+var score = 0;
+var opt1 = document.getElementById('opt1');
+var opt2 = document.getElementById('opt2');
+var opt3 = document.getElementById('opt3');
+var sec = document.getElementById('sec');
 
-var ques = document.getElementById('question')
-var index = 0
-var btn = document.getElementById('btn')
-var score = 0
-var opt1 = document.getElementById('opt1')
-var opt2 = document.getElementById('opt2')
-var opt3 = document.getElementById('opt3')
-var sec = document.getElementById('sec')
+
 
 function nextQuestion() {
-    var options = document.getElementsByName('ans')
-    console.log(options, options.length);
-    for (var i = 0; i < options.length; i++) {
+    var options = document.getElementsByName('ans');
 
+    for (var i = 0; i < options.length; i++) {
         if (options[i].checked) {
-            var selectedNumber = options[i].value
+            var selectedNumber = options[i].value;
             var selectedAnswer = questions[index - 1][`option${selectedNumber}`]
-            var correctOption = questions[index - 1].correctOption
+            var correctOption = questions[index - 1].correctOption;
             if (selectedAnswer == correctOption) {
                 score++
             }
         }
-        options[i].checked = false
-        btn.disabled = true
+        options[i].checked = false;
+        btn.disabled = true;
     }
     if (index > questions.length - 1) {
         console.log((score / questions.length) * 100 + "%")
     }
     else {
-        ques.innerText = questions[index].question
-        opt1.innerText = questions[index].option1
-        opt2.innerText = questions[index].option2
-        opt3.innerText = questions[index].option3
-        index++
+        ques.innerText = questions[index].question;
+        opt1.innerText = questions[index].option1;
+        opt2.innerText = questions[index].option2;
+        opt3.innerText = questions[index].option3;
+        index++;
     }
 }
 
-
-
-
-
-// if (users !== null) {
-//     allUsers = JSON.parse(users)
-// }
-
-
-// function signup(event) {
-
-//     event.preventDefault();
-
-//     let userName = document.getElementById('userName').value;
-//     let email = document.getElementById('email').value;
-//     let password = document.getElementById('password').value;
-
-//     let existingUser = allUsers.find(user => user.email === email);
-//     if (existingUser) {
-//         alert("Email already registered. Please login.");
-//         location.href = 'login.html';
-//         return;
-//     }
-//     let user = {
-//         userName: userName,
-//         email: email,
-//         password: password
-//     }
-
-//     allUsers.push(user);
-//     localStorage.setItem('users', JSON.stringify(allUsers));
-
-//     alert("Sign up successful! Please login.");
-//     location.href = 'login.html';
-// }
-
-// function login() {
-//     let email = document.getElementById('email');
-//     let password = document.getElementById('password');
-//     let filterUser = allUsers.filter(data => data.email === email.value && data.password === password.value);
-
-//     if (filterUser.length) {
-//         alert("user login succesful")
-//         location.href = 'loginPage.html'
-//         nextQuestion();
-//     } else {
-//         alert("password incorrect")
-//     }
-// }
-
 nextQuestion()
 
-
+function skip() {
+    index++;
+    if (index < questions.length) {
+        nextQuestion(index--);
+    } else {
+        console.log("End of questions");
+    }
+}
 function btnClick() {
-    btn.disabled = false
+    btn.disabled = false;
 }
 
 
--- JavaScript/Assignments/Quiz-Application/login.js
+++ JavaScript/Assignments/Quiz-Application/login.js
@@ -5,49 +5,48 @@ if (users !== null) {
     allUsers = JSON.parse(users)
 }
 
-
-
 function signup(event) {
-
     event.preventDefault();
-
     let userName = document.getElementById('userName').value;
     let email = document.getElementById('email').value;
     let password = document.getElementById('password').value;
-
     let existingUser = allUsers.find(user => user.email === email);
-    if (existingUser) {
+
+    if (email === "" || password === "" || userName === "") {
+        alert("Please fill out all the information first");
+    } else if (existingUser) {
         alert("Email already registered. Please login.");
         location.href = 'login.html';
         return;
-    }
-    let user = {
-        userName: userName,
-        email: email,
-        password: password
-    }
-
-    allUsers.push(user);
-    localStorage.setItem('users', JSON.stringify(allUsers));
-
-    alert("Sign up successful! Please login.");
-    location.href = 'login.html';
-}
-
-function login() {
-    let email = document.getElementById('email');
-    let password = document.getElementById('password');
-    let filterUser = allUsers.filter(data => data.email === email.value && data.password === password.value);
-
-    if (filterUser.length) {
-        alert("user login succesful")
-        location.href = 'loginPage.html'
-        nextQuestion();
     } else {
-        alert("password incorrect")
+        let user = {
+            userName: userName,
+            email: email,
+            password: password
+        }
+        allUsers.push(user);
+        localStorage.setItem('users', JSON.stringify(allUsers));
+        alert("Sign up successful! Please login.");
+        location.href = 'login.html';
     }
 }
+function login() {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
 
+    if (email === "" || password === "") {
+        alert("Please fill out all the information first");
+    } else {
+        let filterUser = allUsers.filter(data => data.email === email && data.password === password);
+
+        if (filterUser.length) {
+            alert("User login successful");
+            location.href = 'quiz.html';
+        } else {
+            alert("Incorrect email or password");
+        }
+    }
+}
 
 
 
