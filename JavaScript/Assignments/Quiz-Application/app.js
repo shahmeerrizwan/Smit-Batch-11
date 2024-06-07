7786d9a71df0f8a20acab3f7e9b9857ee8c489cd
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-06-07 23:01:14 +0500

    Quiz-App

--- JavaScript/Assignments/Quiz-Application/app.js
+++ JavaScript/Assignments/Quiz-Application/app.js
@@ -125,3 +125,29 @@ function btnClick() {
 }
 
 
+function countdown(elementName, minutes, seconds) {
+    var element, endTime, hours, mins, msLeft, time;
+
+    function twoDigits(n) {
+        return (n <= 9 ? "0" + n : n);
+    }
+
+    function updateTimer() {
+        msLeft = endTime - (+new Date);
+        if (msLeft < 1000) {
+            element.innerHTML = "Time is up!";
+        } else {
+            time = new Date(msLeft);
+            hours = time.getUTCHours();
+            mins = time.getUTCMinutes();
+            element.innerHTML = (hours ? hours + ':' + twoDigits(mins) : mins) + ':' + twoDigits(time.getUTCSeconds());
+            setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+        }
+    }
+
+    element = document.getElementById(elementName);
+    endTime = (+new Date) + 1000 * (60 * minutes + seconds) + 500;
+    updateTimer();
+}
+
+countdown("ten-countdown", 10, 0);
