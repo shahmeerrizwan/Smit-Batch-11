b7615a2833220f1333d9d29d3d81c443bf5cd4da
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-07-09 15:14:27 +0500

    Working

---
+++ JavaScript/Assignments/Module-1-Hakathoun/firebaseAuth.js
@@ -0,0 +1,99 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBZx2BB9f1MIoRcIAyqO2coUuSpfEEpQyw",
+    authDomain: "expertizo-class.firebaseapp.com",
+    databaseURL: "https://expertizo-class-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "expertizo-class",
+    storageBucket: "expertizo-class.appspot.com",
+    messagingSenderId: "409223184847",
+    appId: "1:409223184847:web:a7ca5d1752ba47f9ba52ba",
+    measurementId: "G-LVF3VP0M8Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function showMessage(message, divId) {
+    var messageDiv = document.getElementById(divId);
+    messageDiv.style.display = "block";
+    messageDiv.innerHTML = message;
+    messageDiv.style.opacity = 1;
+    setTimeout(function () {
+        messageDiv.style.opacity = 0;
+    }, 5000);
+}
+
+const signUp = document.getElementById('submitSignUp');
+signUp.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = document.getElementById('rEmail').value;
+    const password = document.getElementById('rPassword').value;
+    const firstName = document.getElementById('fName').value;
+    const lastName = document.getElementById('lName').value;
+
+    const auth = getAuth();
+    const db = getFirestore();
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            const userData = {
+                email: email,
+                password: password,
+                firstName: firstName,
+                lastName: lastName
+            };
+            showMessage('Account Created Successfully', 'signUpMessage');
+            const docRef = doc(db, "users", user.uid);
+            setDoc(docRef, userData)
+                .then(() => {
+                    window.location.href = 'index.html';
+                })
+                .catch((error) => {
+                    console.error("error writing document", error);
+
+                });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            if (errorCode == 'auth/email-already-in-use') {
+                showMessage('Email Address Already Exists !!!', 'signUpMessage');
+            }
+            else {
+                showMessage('unable to create User', 'signUpMessage');
+            }
+        })
+});
+
+const signIn = document.getElementById('submitSignIn');
+signIn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const auth = getAuth();
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            showMessage('login is successful', 'signInMessage');
+            const user = userCredential.user;
+            localStorage.setItem('loggedInUserId', user.uid);
+            window.location.href = 'home.html';
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            if (errorCode === 'auth/invalid-credential') {
+                showMessage('Incorrect Email or Password', 'signInMessage');
+            }
+            else {
+                showMessage('Account does not Exist', 'signInMessage');
+            }
+        })
+})
---
+++ JavaScript/Assignments/Module-1-Hakathoun/homePage.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore, getDoc, doc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBZx2BB9f1MIoRcIAyqO2coUuSpfEEpQyw",
+    authDomain: "expertizo-class.firebaseapp.com",
+    databaseURL: "https://expertizo-class-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "expertizo-class",
+    storageBucket: "expertizo-class.appspot.com",
+    messagingSenderId: "409223184847",
+    appId: "1:409223184847:web:a7ca5d1752ba47f9ba52ba",
+    measurementId: "G-LVF3VP0M8Q"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const db = getFirestore();
+
+onAuthStateChanged(auth, (user) => {
+    const loggedInUserId = localStorage.getItem('loggedInUserId');
+    if (loggedInUserId) {
+        console.log(user);
+        const docRef = doc(db, "users", loggedInUserId);
+        getDoc(docRef)
+            .then((docSnap) => {
+                if (docSnap.exists()) {
+                    const userData = docSnap.data();
+                    document.getElementById('loggedUserFName').innerText = userData.firstName;
+                    document.getElementById('loggedUserEmail').innerText = userData.email;
+                    document.getElementById('loggedUserLName').innerText = userData.lastName;
+
+                }
+                else {
+                    console.log("no document found matching id")
+                }
+            })
+            .catch((error) => {
+                console.log("Error getting document");
+            })
+    }
+    else {
+        console.log("User Id not Found in Local storage")
+    }
+})
+
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth)
+        .then(() => {
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
+            console.error('Error Signing out:', error);
+        })
+})
