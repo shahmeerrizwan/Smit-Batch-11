3f25c0437fff67ba8c9fe177a7045f4ecd60d220
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-28 22:37:03 +0500

    React

---
+++ React/Project 2/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "./Button/Button";
+
+export default function App() {
+  return (
+    <>
+      <Button title="Login" />
+    </>
+  );
+}
+
---
+++ React/Project 2/src/Button/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button(props) {
+  return (
+    <div>
+      <button>{props.title}</button>
+    </div>
+  )
+}
