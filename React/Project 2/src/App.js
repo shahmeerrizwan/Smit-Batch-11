1fe60d6a7b19b7bb04befd3fcf0e52cc38280df8
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-28 22:48:49 +0500

    Components/Props/Array Lopping

--- React/Project 2/src/App.js
+++ React/Project 2/src/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import Button from "./Button/Button";
 
 export default function App() {
+  let arr = ["allo", "loki", "tamatar"];
+
   return (
     <>
+      <ul>
+        {arr.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
       <Button title="Login" />
     </>
   );
 }
-
