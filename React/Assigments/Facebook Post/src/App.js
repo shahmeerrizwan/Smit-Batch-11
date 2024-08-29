f41a332279a78a3cd5627b98460d434a9d01ccd3
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-29 14:26:59 +0500

    Facebook Post

--- React/Assigments/Facebook Post/src/App.js
+++ React/Assigments/Facebook Post/src/App.js
@@ -4,7 +4,7 @@ import FBPost from '../src/Component/FB Post/FBPost'
 export default function App() {
   return (
     <>
-      <FBPost name="Muhammad Shahmeer Rizwan" img="https://avatars.githubusercontent.com/u/148860820?v=4" />
+      <FBPost name="Muhammad Shahmeer Rizwan" img="https://avatars.githubusercontent.com/u/148860820?v=4" postimg="https://scontent.fkhi2-2.fna.fbcdn.net/v/t39.30808-6/420072792_351393754457004_8814050884371759703_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=833d8c&_nc_ohc=gRI-T1cjkysQ7kNvgGw2zRD&_nc_ht=scontent.fkhi2-2.fna&oh=00_AYBTWeejZhfNflepn5EYyd24JSJCnNjH8RH5tEIlR9gpbA&oe=66D60913" />
     </>
   );
 }
