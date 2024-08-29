bc6f4044a17567b462a73c22b20fed547f3b9f3c
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-29 14:19:43 +0500

    Facebook Post

--- React/Assigments/Facebook Post/src/Component/FB Post/FBPost.js
+++ React/Assigments/Facebook Post/src/Component/FB Post/FBPost.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.css'
 
-export default function index() {
+export default function index(props) {
     return (
         <>
             <div className="container mt-5 mb-5">
@@ -9,19 +9,17 @@ export default function index() {
                     <div className="col-md-6">
                         <div className="card">
                             <div className="d-flex justify-content-between p-2 px-3">
-                                <div className="d-flex flex-row align-items-center"> <img src="https://i.imgur.com/UXdKE3o.jpg" width="50" className="rounded-circle" alt='...' />
-                                    <div className="d-flex flex-column ml-2"> <span className="font-weight-bold">Jeanette Sun</span> <small className="text-primary">Collegues</small> </div>
+                                <div className="d-flex flex-row align-items-center"> <img src={props.img} width="50" className="rounded-circle" alt='...' />
+                                    <div className="d-flex flex-column ml-2"> <span className="font-weight-bold">{props.name}</span> <small className="text-primary">SMIT</small> </div>
                                 </div>
                                 <div className="d-flex flex-row mt-1 ellipsis"> <small className="mr-2">20 mins</small> <i className="fa fa-ellipsis-h"></i> </div>
-                            </div> <img src="https://i.imgur.com/xhzhaGA.jpg" className="img-fluid" alt='...' />
+                            </div> <img src={props.postimg} className="img-fluid" alt='...' />
                             <div className="p-2">
                                 <div className="d-flex justify-content-between align-items-center">
                                     <div className="d-flex flex-row icons d-flex align-items-center"> <i className="fa fa-heart"></i> <i className="fa fa-smile-o ml-2"></i> </div>
                                     <div className="d-flex flex-row muted-color"> <span>2 comments</span> <span className="ml-2">Share</span> </div>
                                 </div>
                                 <hr />
-
-
                                 <div className="comment-input">
                                     <div className="fonts"> <i className="fa fa-camera"></i> </div>
                                     <input type="text" className="form-control" />
