2973ef1a8e61e02d0b132393ce5befe39227c2d4
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-29 14:21:16 +0500

    Facebook Post

--- React/Assigments/Facebook Post/src/App.js
+++ React/Assigments/Facebook Post/src/App.js
@@ -4,7 +4,7 @@ import FBPost from '../src/Component/FB Post/FBPost'
 export default function App() {
   return (
     <>
-      <FBPost />
+      <FBPost name="Muhammad Shahmeer Rizwan" img="https://avatars.githubusercontent.com/u/148860820?v=4" />
     </>
   );
 }
