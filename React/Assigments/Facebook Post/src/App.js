f8a1ac5e5b9dc932a6bb095e4df9353cbfdd7a54
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2024-08-29 14:15:25 +0500

    Facebook Post

--- React/Assigments/Facebook Post/src/App.js
+++ React/Assigments/Facebook Post/src/App.js
@@ -1,22 +1,11 @@
 import React from "react";
+import FBPost from '../src/Component/FB Post/FBPost'
 
 export default function App() {
   return (
     <>
-      <h1>My First Project </h1>
-      <img
-        src="https://media.licdn.com/dms/image/sync/v2/D4D10AQGbP-e0Bd-d6w/image-shrink_800/image-shrink_800/0/1721337927638/AZRDIR_LI-PAYG-Grey-5_IND_1200x1200_LIA_AZR-Direct_EN_Trial_Standard_SBAN_SINU_Illustration_Benefitpng?e=1725444000&v=beta&t=Qw2dyyxzA8Xu2hkl6tmsYWdwSHYsUYz8PvwpP1j_hZ0"
-        alt=""
-      />
-      <div className="para">
-        <p>
-          Lorem ipsum lorem loris amdun jik pokmen and uinme men in the aeartgn
-        </p>
-      </div>
-
-      <input type="text" placeholder="enter text" />
+      <FBPost />
     </>
   );
 }
 
-// React is a library
---
+++ React/Assigments/Facebook Post/src/Component/FB Post/FBPost.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './style.css'
+
+export default function index() {
+    return (
+        <>
+            <div className="container mt-5 mb-5">
+                <div className="row d-flex align-items-center justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="d-flex justify-content-between p-2 px-3">
+                                <div className="d-flex flex-row align-items-center"> <img src="https://i.imgur.com/UXdKE3o.jpg" width="50" className="rounded-circle" alt='...' />
+                                    <div className="d-flex flex-column ml-2"> <span className="font-weight-bold">Jeanette Sun</span> <small className="text-primary">Collegues</small> </div>
+                                </div>
+                                <div className="d-flex flex-row mt-1 ellipsis"> <small className="mr-2">20 mins</small> <i className="fa fa-ellipsis-h"></i> </div>
+                            </div> <img src="https://i.imgur.com/xhzhaGA.jpg" className="img-fluid" alt='...' />
+                            <div className="p-2">
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="d-flex flex-row icons d-flex align-items-center"> <i className="fa fa-heart"></i> <i className="fa fa-smile-o ml-2"></i> </div>
+                                    <div className="d-flex flex-row muted-color"> <span>2 comments</span> <span className="ml-2">Share</span> </div>
+                                </div>
+                                <hr />
+
+
+                                <div className="comment-input">
+                                    <div className="fonts"> <i className="fa fa-camera"></i> </div>
+                                    <input type="text" className="form-control" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        </>
+    )
+}
