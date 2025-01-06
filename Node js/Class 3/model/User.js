9c972c44b3e9e2dfe7504824a54bcd5508d6bce1
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2025-01-07 00:22:24 +0500

    JWT Authentication

---
+++ Node js/Class 3/model/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    userName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: string, required: true },
+    verifiedEmail: { type: Boolean, default: false },
+})
+
+const User = mongoose.model("Users", userSchema);
+
+export default User;
---
+++ Node js/Class 3/routes/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import user from './users.js'
+
+
+const router = express.Router()
+router.use('/user', user);
+
+export default router;
