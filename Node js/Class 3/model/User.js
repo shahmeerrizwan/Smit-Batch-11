3ba62521532c51363b4566f130645a966249d791
shahmeerrizwan <shahmeerrizwan921@gmail.com>
2025-01-14 14:16:35 +0500

    JWT Authentication

--- Node js/Class 3/model/User.js
+++ Node js/Class 3/model/User.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
     userName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    token: { type: String, required: true },
     verifiedEmail: { type: Boolean, default: false },
 })
 
